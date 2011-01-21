7dd35a669c3ddef119c539f9cf6e81c95ec75ed2
Will Thompson <will@willthompson.co.uk>
2011-01-21 16:00:59 +0100

    Refactor event list to its own class

--- sojourner.py
+++ sojourner.py
@@ -27,82 +27,14 @@ from pynotify import Notification
 from malvern import *
 from sojourner.updater import Updater
 from sojourner.schedule import Schedule
+from sojourner.eventlist import EventList
 
 class Thing:
-    def toggle_toggled(self, toggle, event, update_star):
-        if toggle.get_active():
-            self.schedule.add_favourite(event)
-            update_star(True)
-        else:
-            self.schedule.remove_favourite(event)
-            update_star(False)
-
-    def event_activated(self, treeview, row, column):
-        store = treeview.get_property('model')
-        iter = store.get_iter(row)
-        event, = store.get(iter, 1)
-
-        window = MaybeStackableWindow(event.title)
-
-        vbox = gtk.VBox(spacing=12)
-
-        label = gtk.Label()
-        label.set_markup(event.full())
-        label.set_properties(wrap=True, justify=gtk.JUSTIFY_FILL)
-        vbox.pack_start(label)
-
-        def update_star(state):
-            store.set(iter, 2, state)
-
-        toggle = MagicCheckButton("Favourite")
-        toggle.set_active(event in self.schedule.favourites)
-        toggle.connect('toggled', self.toggle_toggled, event, update_star)
-        vbox.pack_start(toggle, False)
-
-        pannable = MaybePannableArea()
-        pannable.add_with_viewport(vbox)
-        window.add(pannable)
-
-        window.show_all()
-
-    def event_list(self, title, events):
-        window = MaybeStackableWindow(title)
-        store = gtk.TreeStore(str, object, bool)
-
-        for event in events:
-            store.append(None, [event.summary(), event,
-                                event in self.schedule.favourites])
-
-        treeview = gtk.TreeView(store)
-        treeview.set_headers_visible(False)
-        treeview.connect("row-activated", self.event_activated)
-
-        tvcolumn = gtk.TreeViewColumn('Stuff')
-        treeview.append_column(tvcolumn)
-
-        cell = gtk.CellRendererText()
-        cell.set_property("ellipsize", pango.ELLIPSIZE_END)
-        tvcolumn.pack_start(cell, True)
-        tvcolumn.add_attribute(cell, 'markup', 0)
-
-        cell = gtk.CellRendererPixbuf()
-        # ou! \o/
-        cell.set_property("icon-name",
-            "imageviewer_favourite" if have_hildon else "emblem-special")
-        tvcolumn.pack_start(cell, False)
-        tvcolumn.add_attribute(cell, 'visible', 2)
-
-        pannable = MaybePannableArea()
-        pannable.add(treeview)
-        window.add(pannable)
-
-        window.show_all()
-
     def blah_activated(self, treeview, row, column, title, d):
         store = treeview.get_property('model')
         blah, = store.get(store.get_iter(row), 0)
 
-        self.event_list(blah, d[blah])
+        EventList(self.schedule, blah, d[blah])
 
     def by_blah(self, d, title):
         window = MaybeStackableWindow(title)
@@ -132,13 +64,13 @@ class Thing:
 
     def view_activated(self, treeview, row, column):
         if row[0] == 0:
-            self.event_list("All events", self.schedule.events)
+            EventList(self.schedule, "All events", self.schedule.events)
         elif row[0] == 1:
             self.by_blah(self.schedule.events_by_room, "Rooms")
         elif row[0] == 2:
             self.by_blah(self.schedule.events_by_track, "Tracks")
         elif row[0] == 3:
-            self.event_list("Favourites", self.schedule.favourites)
+            EventList(self.schedule, "Favourites", self.schedule.favourites)
         else:
             print row
 
---
+++ sojourner/eventlist.py
@@ -0,0 +1,80 @@
+import gtk
+import pango
+from malvern import (
+    MaybeStackableWindow, MaybePannableArea, MagicCheckButton,
+    have_hildon,
+)
+
+class EventList(MaybeStackableWindow):
+    """Shows a list of events; clicking on an event shows details of that
+    event."""
+    def __init__(self, schedule, title, events):
+        MaybeStackableWindow.__init__(self, title)
+        self.schedule = schedule
+        store = gtk.TreeStore(str, object, bool)
+
+        for event in events:
+            store.append(None, [event.summary(), event,
+                                event in self.schedule.favourites])
+
+        treeview = gtk.TreeView(store)
+        treeview.set_headers_visible(False)
+        treeview.connect("row-activated", self.event_activated)
+
+        tvcolumn = gtk.TreeViewColumn('Stuff')
+        treeview.append_column(tvcolumn)
+
+        cell = gtk.CellRendererText()
+        cell.set_property("ellipsize", pango.ELLIPSIZE_END)
+        tvcolumn.pack_start(cell, True)
+        tvcolumn.add_attribute(cell, 'markup', 0)
+
+        cell = gtk.CellRendererPixbuf()
+        # ou! \o/
+        cell.set_property("icon-name",
+            "imageviewer_favourite" if have_hildon else "emblem-special")
+        tvcolumn.pack_start(cell, False)
+        tvcolumn.add_attribute(cell, 'visible', 2)
+
+        pannable = MaybePannableArea()
+        pannable.add(treeview)
+        self.add(pannable)
+
+        self.show_all()
+
+    def event_activated(self, treeview, row, column):
+        store = treeview.get_property('model')
+        iter = store.get_iter(row)
+        event, = store.get(iter, 1)
+
+        window = MaybeStackableWindow(event.title)
+
+        vbox = gtk.VBox(spacing=12)
+
+        label = gtk.Label()
+        label.set_markup(event.full())
+        label.set_properties(wrap=True, justify=gtk.JUSTIFY_FILL)
+        vbox.pack_start(label)
+
+        def update_star(state):
+            store.set(iter, 2, state)
+
+        toggle = MagicCheckButton("Favourite")
+        toggle.set_active(event in self.schedule.favourites)
+        toggle.connect('toggled', self.toggle_toggled, event, update_star)
+        vbox.pack_start(toggle, False)
+
+        pannable = MaybePannableArea()
+        pannable.add_with_viewport(vbox)
+        window.add(pannable)
+
+        window.show_all()
+
+    def toggle_toggled(self, toggle, event, update_star):
+        if toggle.get_active():
+            self.schedule.add_favourite(event)
+            update_star(True)
+        else:
+            self.schedule.remove_favourite(event)
+            update_star(False)
+
