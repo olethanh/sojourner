2fbaa218a6c0d7a83878bcc2441e6f651d0d130e
Will Thompson <will@willthompson.co.uk>
2011-01-22 19:31:38 +0100

    Include more details in by-category lists

--- sojourner/categorylist.py
+++ sojourner/categorylist.py
@@ -1,19 +1,50 @@
+# vim: set fileencoding=utf-8 sts=4 sw=4 :
+
+from itertools import groupby
 import gtk
 import pango
-from malvern import MaybeStackableWindow, MaybePannableArea
+from malvern import MaybeStackableWindow, MaybePannableArea, esc
 from sojourner.eventlist import EventList
+
+def summarize_events(events):
+    """Given a list of events, returns a summary of how many there are, plus
+    the time ranges on each day."""
+
+    def format_group(day, day_event_iter):
+        day_events = list(day_event_iter)
+        return "%s %s–%s" % (day, day_events[0].start, day_events[-1].end)
+
+    time_summary = ', '.join(
+        format_group(day, event_iter)
+        for day, event_iter in groupby(events, lambda e: e.date)
+        )
+
+    return "%(n)u events: %(time_summary)s" % {
+        'n': len(events),
+        'time_summary': time_summary,
+    }
 
 class CategoryList(MaybeStackableWindow):
     COL_CATEGORY = 0
+    COL_EVENTS = 1
+    COL_CATEGORY_SUMMARY = 2
 
     def __init__(self, schedule, title, categories):
         MaybeStackableWindow.__init__(self, title)
         self.schedule = schedule
         self.categories = categories
-        self.store = gtk.TreeStore(str)
+        # This should really be   (str, list) but that doesn't seem to work:
+        #   TypeError: could not get typecode from object
+        # I guess list is not a subclass of object.
+        self.store = gtk.TreeStore(str, object, str)
 
-        for room in sorted(categories.keys()):
-            self.store.append(None, [room])
+        for category, events in sorted(categories.items()):
+            summary = """<b>%(category)s</b>
+<small>%(event_summary)s</small>""" % {
+                'category': esc(category),
+                'event_summary': summarize_events(events),
+            }
+            self.store.append(None, (category, events, summary))
 
         treeview = gtk.TreeView(self.store)
         treeview.set_headers_visible(False)
@@ -26,7 +57,8 @@ class CategoryList(MaybeStackableWindow):
         cell.set_property("ellipsize", pango.ELLIPSIZE_END)
         tvcolumn.pack_start(cell, True)
 
-        tvcolumn.add_attribute(cell, 'markup', CategoryList.COL_CATEGORY)
+        tvcolumn.add_attribute(cell, 'markup',
+            CategoryList.COL_CATEGORY_SUMMARY)
 
         pannable = MaybePannableArea()
         pannable.add(treeview)
@@ -36,6 +68,7 @@ class CategoryList(MaybeStackableWindow):
 
     def category_activated(self, treeview, row, column):
         i = self.store.get_iter(row)
-        category, = self.store.get(i, CategoryList.COL_CATEGORY)
+        category, events = self.store.get(i,
+            CategoryList.COL_CATEGORY, CategoryList.COL_EVENTS)
 
-        EventList(self.schedule, category, self.categories[category])
+        EventList(self.schedule, category, events)
