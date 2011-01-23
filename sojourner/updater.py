5d61a0a1062731057fad39c955612f8c95d37829
Will Thompson <will@willthompson.co.uk>
2011-01-23 13:05:03 +0000

    Updater: run everything in the worker thread
    Previously only the HTTP download was run in a thread; everything else
    (deleting the temporary file, copying the new file, etc.) was done in
    the main thread.
    
    This made the code a bit harder to follow; it also won't fly if we want
    to parse the schedule in a thread, too.

--- sojourner/updater.py
+++ sojourner/updater.py
@@ -27,81 +27,82 @@ class Updater(gtk.Dialog):
         gtk.Dialog.__init__(self, title='Downloading the latest schedule…',
             parent=parent, buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL))
 
-        self._progress = gtk.ProgressBar()
-        self._progress.set_fraction(0.0)
+        self.__progress = gtk.ProgressBar()
+        self.__progress.set_fraction(0.0)
+        self.vbox.pack_start(self.__progress)
 
-        self.vbox.pack_start(self._progress)
+        self.connect('response', Updater.__response_cb)
 
-        self.connect('response', Updater._response_cb)
-
-        # Kick off the actual copying
-        self._source = gio.File(url)
-        self._temp = gio.File('/tmp/' + target.get_basename())
-        self._target = target
-
-        # Setting flags=gio.FILE_COPY_OVERWRITE apparently doesn't work on
-        # copy_async so we have to do this stupid dance with downloading to
-        # /tmp and then overwriting.
-        try:
-            self._temp.delete()
-        except Exception, e:
-            pass
-
-        self._cancellable = gio.Cancellable()
-        # Maemo 5's pygobject doesn't have copy_async. It seems to have copy,
-        # so we can use a thread to basically re-implement copy_async...
-        self._download_thread = threading.Thread(target=self.start_copying,
-            name='Downloader')
-        self._download_thread.daemon = True
-        self._download_thread.start()
+        # Stash urls and things
+        self.__source = gio.File(url)
+        self.__temp = gio.File('/tmp/' + target.get_basename())
+        self.__target = target
+        self.__finished_cb = finished_cb
 
         # Throb for a while until the file is actively being downloaded.
-        self._pulse_timeout = glib.timeout_add(100, self._pulse_cb)
+        self.__pulse_timeout = 0
+        self.__start_pulsing()
 
-        self._finished_cb = finished_cb
+        # Kick off the actual copying.  Maemo 5's pygobject doesn't have
+        # copy_async, so we use a thread to basically re-implement
+        # copy_async...
+        self.__cancellable = gio.Cancellable()
+        self.__download_thread = threading.Thread(target=self.__download,
+            name='Downloader')
+        self.__download_thread.daemon = True
+        self.__download_thread.start()
 
-    def start_copying(self):
+    def __download(self):
+        """Grabs the latest schedule, if possible."""
         try:
-            self._source.copy(self._temp,
-                (lambda c, t: glib.idle_add(self._progress_cb, c, t)),
+            self.__source.copy(self.__temp,
+                # Use an idle to be run in the main thread.
+                (lambda c, t: glib.idle_add(self.__progress_cb, c, t)),
                 flags=gio.FILE_COPY_OVERWRITE,
-                cancellable=self._cancellable)
-            glib.idle_add(self._finished_copying, None)
+                cancellable=self.__cancellable)
+
+            # FIXME: parse the new file to make sure it's actually valid before
+            # overwriting any existing file.
+            self.__temp.move(self.__target, flags=gio.FILE_COPY_OVERWRITE)
+
+            glib.idle_add(self.__finished_copying, None)
         except Exception, e:
-            glib.idle_add(self._finished_copying, e)
+            glib.idle_add(self.__finished_copying, e)
 
-    def _pulse_cb(self):
-        self._progress.pulse()
-        return True
-
-    def _progress_cb(self, current_bytes, total_bytes):
-        self._stop_pulsing()
+    def __progress_cb(self, current_bytes, total_bytes):
+        """Called (in an idle, so in the glib mainloop thread) for each chunk
+        of data downloaded."""
+        self.__stop_pulsing()
 
         if total_bytes == 0:
-            # This is relatively hypothetical
-            self._progress.pulse()
+            # The server didn't tell us how big the file is; so the best we
+            # can do is pulse for each chunk of data.
+            self.__progress.pulse()
         else:
             fraction = float(current_bytes) / total_bytes
-            self._progress.set_fraction(fraction)
+            self.__progress.set_fraction(fraction)
 
-    def _finished_copying(self, e):
-        self._stop_pulsing()
+    def __finished_copying(self, e):
+        self.__stop_pulsing()
+        self.__progress.set_fraction(1.0)
+        self.__finished_cb(self, e)
 
-        if e is not None:
-            self._finished_cb(self, e)
-        else:
-            try:
-                # flags= seems to work here...
-                self._temp.move(self._target, flags=gio.FILE_COPY_OVERWRITE)
-                self._progress.set_fraction(1.0)
-                self._finished_cb(self, None)
-            except gio.Error, e:
-                self._finished_cb(self, e)
+    def __start_pulsing(self):
+        if self.__pulse_timeout == 0:
+            self.__pulse_timeout = glib.timeout_add(100, self.__pulse_cb)
 
-    def _stop_pulsing(self):
-        if self._pulse_timeout != 0:
-            glib.source_remove(self._pulse_timeout)
-            self._pulse_timeout = 0
+        return False
 
-    def _response_cb(self, response_id):
-        self._cancellable.cancel()
+    def __pulse_cb(self):
+        self.__progress.pulse()
+        return True
+
+    def __stop_pulsing(self):
+        if self.__pulse_timeout != 0:
+            glib.source_remove(self.__pulse_timeout)
+            self.__pulse_timeout = 0
+
+        return False
+
+    def __response_cb(self, response_id):
+        self.__cancellable.cancel()
