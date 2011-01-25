8f2ba552ad0709250d32c1d63d8b3527bd8bda57
Will Thompson <will@willthompson.co.uk>
2011-01-25 00:24:00 +0000

    Add launcher icons, and install desktop file.

--- setup.py
+++ setup.py
@@ -10,7 +10,13 @@ setup(
     packages=['sojourner'],
     scripts=['bin/sojourner'],
     py_modules=['sojourner'],
-    data_files=[('share/sojourner', ['banner.png'])],
+    data_files=[
+        ('share/sojourner', ['banner.png']),
+        ('share/icons/hicolor/48x48/hildon', ['icons/48x48/sojourner.png']),
+        # Yes, other apps put 64x64 icons in 'scalable'.
+        ('share/icons/hicolor/scalable/hildon', ['icons/64x64/sojourner.png']),
+        ('share/applications/hildon', ['sojourner.desktop']),
+    ],
     license='GPL v3',
 )
 
